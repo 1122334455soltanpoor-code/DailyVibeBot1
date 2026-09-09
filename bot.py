b07850940ab6dbfead95f58e695e2aed46338d8d
1122334455soltanpoor-code <1122334455soltanpoor@gmail.com>
2026-09-10 00:45:25 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,100 +1,16 @@
 import os
-import asyncio
-from datetime import datetime
-from zoneinfo import ZoneInfo
-
-from telegram import Bot
-from telegram.constants import ChatPermissions
-
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
 
-IRAN_TZ = ZoneInfo("Asia/Tehran")
+async def get_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(str(update.effective_chat.id))
 
-
-async def set_day_mode(bot):
-    permissions = ChatPermissions(
-        can_send_messages=True,
-        can_send_photos=True,
-        can_send_videos=False,
-        can_send_audios=False,
-        can_send_documents=False,
-        can_send_voice_notes=False,
-        can_send_video_notes=False,
-        can_send_polls=False,
-        can_send_other_messages=False,
-        can_add_web_page_previews=False,
-        can_change_info=False,
-        can_invite_users=False,
-        can_pin_messages=False,
-    )
-
-    await bot.set_chat_permissions(
-        chat_id=CHAT_ID,
-        permissions=permissions,
-        use_independent_chat_permissions=True,
-    )
-
-    print("DAY MODE: فقط عکس")
-
-
-async def set_night_mode(bot):
-    permissions = ChatPermissions(
-        can_send_messages=True,
-        can_send_photos=False,
-        can_send_videos=False,
-        can_send_audios=False,
-        can_send_documents=False,
-        can_send_voice_notes=False,
-        can_send_video_notes=False,
-        can_send_polls=False,
-        can_send_other_messages=False,
-        can_add_web_page_previews=False,
-        can_change_info=False,
-        can_invite_users=False,
-        can_pin_messages=False,
-    )
-
-    await bot.set_chat_permissions(
-        chat_id=CHAT_ID,
-        permissions=permissions,
-        use_independent_chat_permissions=True,
-    )
-
-    print("NIGHT MODE: فقط متن")
-
-
-async def main():
-    if not TOKEN:
-        raise ValueError("BOT_TOKEN تنظیم نشده")
-
-    if not CHAT_ID:
-        raise ValueError("CHAT_ID تنظیم نشده")
-
-    bot = Bot(TOKEN)
-    last_mode = None
-
-    while True:
-        now = datetime.now(IRAN_TZ)
-        hour = now.hour
-
-        # 07:00 تا 23:59 = عکس
-        if 7 <= hour < 24:
-            mode = "day"
-        else:
-            mode = "night"
-
-        if mode != last_mode:
-            if mode == "day":
-                await set_day_mode(bot)
-            else:
-                await set_night_mode(bot)
-
-            last_mode = mode
-
-        await asyncio.sleep(30)
-
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("id", get_id))
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
